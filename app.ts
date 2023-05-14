5ef049cbf2e53b3c082d0486a7838f0e28522f5e
louazz <louaizaiter@gmail.com>
2023-05-14 15:59:44 +0100

    changes

--- app.ts
+++ app.ts
@@ -7,7 +7,7 @@ const app = new Application();
 const PORT = 8000;
 
 app.use(oakCors({
-    origin: false,
+    origin: true,
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   }));
 app.use(router.routes());
