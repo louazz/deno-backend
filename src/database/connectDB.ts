ed859ffed876e157c254da2122c787c3dc1aa41e
louazz <louaizaiter@gmail.com>
2023-05-14 12:19:48 +0100

    change

--- src/database/connectDB.ts
+++ src/database/connectDB.ts
@@ -2,11 +2,11 @@ import { MongoClient } from "https://deno.land/x/mongo@v0.30.0/mod.ts";
 
 const client = new MongoClient();
 
-const dbString= "mongodb://127.0.0.1:27017";
+const dbString= "mongodb://mongo:27017";
 
 await client.connect(dbString);
 
 console.log("Database connected!");
 
-const db= client.database("app");
+const db= client.database("admin");
 export default db;
